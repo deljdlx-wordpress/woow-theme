b7031a15c2e9bfc9cda40acd41b2b7d82e6102f3
jdlxt <jdlxt@localhost>
2021-04-27 13:30:27 +0200

    layout

--- partials/sections/bootstrap.php
+++ partials/sections/bootstrap.php
@@ -3,19 +3,24 @@
          <h2>Bootstrap components demo</h2>
 
          <div class="subsection">
-            <?php
-            get_template_part('partials/sections/_bootstrap/navbar');
-            ?>
+
+            <div class="col-12">
+                <?php
+                get_template_part('partials/sections/_bootstrap/navbar');
+                ?>
+            </div>
+
         </div>
 
         <div class="subsection">
-            <?php
-            get_template_part('partials/sections/_bootstrap/carousel');
-            ?>
-        </div>
-
         <div class="row">
             <div class="col-6">
+                <?php
+                get_template_part('partials/sections/_bootstrap/carousel');
+                ?>
+            </div>
+
+            <div class="col-3">
                 <div class="subsection">
                     <?php
                     get_template_part('partials/sections/_bootstrap/buttons');
@@ -23,7 +28,7 @@
                 </div>
             </div>
 
-            <div class="col-6">
+            <div class="col-3">
                 <div class="subsection">
                     <?php
                     get_template_part('partials/sections/_bootstrap/modal');
@@ -31,6 +36,7 @@
                 </div>
             </div>
         </div>
+
 
         <div class="row">
             <div class="col-6">
